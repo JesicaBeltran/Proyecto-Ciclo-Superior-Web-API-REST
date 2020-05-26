0219b2d36279fb9747ffd2314e67eabe73082750
Jesica <jessbc@gmail.com>
2020-05-26 17:51:57 +0200

    Terminado buscar por producto

--- Supermercados/Services/AvisoServices.cs
+++ Supermercados/Services/AvisoServices.cs
@@ -1,10 +1,16 @@
 ﻿using AvisoRepository.Repository;
 using AvisoServices.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Web.Http.Controllers;
 
 namespace AvisoServices.Services
 {
@@ -18,14 +24,116 @@ namespace AvisoServices.Services
             _avisoItems = new Aviso();
             _avisoRepository = avisoRepository;
         }
+        //VER TODOS LOS AVISOS
         public IEnumerable<Aviso> GetAvisoItems()
         {
             return _avisoRepository.GetAvisoItems();
         }
-
-        /*public void AddAvisoItems(Aviso items)
+        //VER POR SUPERMERCADO
+        public IEnumerable<Aviso> GetAvisoSupermercado(string localidad, string supermercado)
         {
+            //operaciones
+            var todosAvisos = _avisoRepository.GetAvisoItems();
+            supermercado.ToUpperInvariant();
+
+            var encontrados = new List<Aviso>();
+
+            foreach (var buscado in todosAvisos)
+            {
+                if (buscado.Localidad==localidad && buscado.Supermercado==supermercado)
+                {
+                    encontrados.Add(buscado);
+                }
+            }
+
+            return encontrados;
+        }
+
+        //VER POR PRODUCTO
+        public IEnumerable<Aviso> GetAvisoProducto(string localidad, string producto)
+        {
+            //operaciones
+            var todosAvisos = _avisoRepository.GetAvisoItems();
+
+            var encontrados = new List<Aviso>();
            
-        }*/
+            foreach (var buscado in todosAvisos)
+            {
+                if (buscado.Localidad == localidad && buscado.Producto.Contains(producto))
+                {
+                    encontrados.Add(buscado);
+                }
+            }
+
+            return encontrados;
+        }
+
+
+
+        //VER AVISOS CON BUSCADOR POR SUPER/PRODUCTO
+        public IEnumerable<Aviso> GetAvisoItemsBuscador(string palabraClave)
+        {
+            
+            var todosAvisos = _avisoRepository.GetAvisoItems();
+
+            var encontrados = new List<Aviso>();
+
+            foreach (var buscado in todosAvisos)
+            {
+                if (buscado.Producto.Contains(palabraClave) || buscado.Supermercado.Contains(palabraClave))
+                {
+                    encontrados.Add(buscado);
+                }
+            }
+          
+            return encontrados;
+
+        }
+        
+        //Ordenar avisos
+        public IEnumerable<Aviso> GetAvisoItemsOrden(string orden)
+        {
+
+            var todosAvisos = _avisoRepository.GetAvisoItems();
+
+            var avisosOrden = new List<Aviso>();
+
+            switch (orden)
+            {
+                case "fecha":
+                    foreach (var buscado in todosAvisos)
+                    {
+                        avisosOrden.Add(buscado);
+                    }
+                    avisosOrden.Sort(
+                         delegate (Aviso x, Aviso y)
+                         {
+                             return y.FechaCreacion.CompareTo(x.FechaCreacion);
+                         });
+
+
+                    return avisosOrden;
+            
+                case "alfabetico":
+                    foreach (var buscado in todosAvisos)
+                    {
+                        avisosOrden.Add(buscado);
+                    }
+                    var ordenada=avisosOrden.OrderBy(f => f.Producto).ToList();
+                    return ordenada;
+
+                default:
+                    return avisosOrden;
+            }
+        }
+        //AÑADIR AVISO
+        public async Task AddAvisoItems(Aviso items)
+        {
+            items.FechaCreacion = DateTime.Now;
+
+            await _avisoRepository.AddAvisoItems(items);
+            
+        }
+
     }
 }
--- Supermercados/Services/IAvisoServices.cs
+++ Supermercados/Services/IAvisoServices.cs
@@ -1,12 +1,17 @@
 ﻿using AvisoServices.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AvisoServices.Services
 {
    public interface IAvisoServices
     {
-        //public void AddAvisoItems(Aviso items);
+        Task AddAvisoItems(Aviso items);
         IEnumerable<Aviso> GetAvisoItems();
+        IEnumerable<Aviso> GetAvisoSupermercado(string localidad, string supermercado);
+        IEnumerable<Aviso> GetAvisoProducto(string localidad, string producto);
+        IEnumerable<Aviso> GetAvisoItemsBuscador(string palabraClave);
+        IEnumerable<Aviso> GetAvisoItemsOrden(string orden);
     }
 }
